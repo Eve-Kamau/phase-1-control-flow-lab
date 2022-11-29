033e23a748aa1f8a17754c47c52e874935b649f0
Eve-Kamau <evelyne.kamau@student.moringaschool.com>
2022-11-30 00:31:16 +0300

    commit tree 37645cdc12028acd45c76cd0c4b15799c2153c84

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
 
-function ternaryCheckCity(){
+function scuberGreetingForFeet(rideMiles){
   // Write your code here!
+  
+if(rideMiles <= 400){
+  return "This one is on me!";
+} 
+else if(rideMiles > 2500){
+  return "No can do.";
 }
+else {
+  return "I will gladly take your thirty bucks.";
+} 
+}
+console.log(scuberGreetingForFeet(1300));
 
-function switchOnCharmFromTip(){
+let usCity = 'NYC';
+function ternaryCheckCity (usCity){
   // Write your code here!
+return usCity === 'NYC'?'Ok, sounds good.' : 'No go.'
+}
+console.log(usCity(Nairobi));
+
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  switch(tip){
+    case 'generous':
+    return 'Thank you so much.'
+    case 'not as generous':
+    return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
 }
